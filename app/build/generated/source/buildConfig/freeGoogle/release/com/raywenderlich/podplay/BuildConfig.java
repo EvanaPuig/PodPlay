ae3fde700622d9e530efa48a5d152c800a909578
Evana Margain Puig <evanamp233@gmail.com>
2021-07-24 19:48:27 -0500

    Initial commit

---
+++ app/build/generated/source/buildConfig/freeGoogle/release/com/raywenderlich/podplay/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.raywenderlich.podplay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.raywenderlich.podplay.evana";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "freeGoogle";
+  public static final String FLAVOR_version_type = "free";
+  public static final String FLAVOR_store = "google";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0-free-google";
+  // Field from default config.
+  public static final boolean IS_PRODUCTION = false;
+}
